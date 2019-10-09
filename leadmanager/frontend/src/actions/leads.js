4e5af61d07b0e62085fb9227d8da5474e973c621
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-10-08 22:11:34 -0400

    added login/logout app

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { GET_LEADS, DELETE_LEAD, CREATE_LEAD, GET_ERRORS } from './types';
-import { createMessage } from './messages';
+import { createMessage, returnErrors} from './messages';
 
 export const getLeads = () => dispatch => {
   axios.get('/api/leads/')
@@ -9,7 +9,9 @@ export const getLeads = () => dispatch => {
         type: GET_LEADS,
         payload: res.data
       });
-    }).catch(err => console.log(err));
+    }).catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status))
+    });
 };
 
 export const createLead = (lead) => dispatch => {
@@ -21,15 +23,7 @@ export const createLead = (lead) => dispatch => {
         payload: res.data
       });
     }).catch(err => {
-      const errors = {
-        msg: err.response.data,
-        status: err.response.status
-      };
-      
-      dispatch({
-        type: GET_ERRORS,
-        payload: errors
-      });
+      dispatch(returnErrors(err.response.data, err.response.status));
     });
 };
 
